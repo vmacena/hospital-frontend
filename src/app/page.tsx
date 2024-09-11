281086408757dda9b208a20b230250b04341f031
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-09-11 12:41:31 -0300

    feat: create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import React from 'react';
+import styles from './page.module.scss';
+
 export default function Page() {
     return (
         <html lang="en">
         <body>
-            <h1>Page</h1>
+            <h1 className={styles.test} >Page</h1>
         </body>
         </html>
     );
