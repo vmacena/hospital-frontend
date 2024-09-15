7998904dfa0d9e8436a7162f84ccc3583cede54f
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-09-15 19:36:53 -0300

    resolved conflicts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-<<<<<<< HEAD
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import { ToastContainer } from 'react-toastify';
@@ -10,15 +9,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Hospital Universitário",
   description: "Entre morto e saia vivo",
-=======
-import "./globals.scss";
-import Header from './components/header/Header';
-import headerConfig from './config/header/headerConfig.json';
-
-export const metadata: Metadata = {
-  title: "Hospital Universitário de São Carlos",
-  description: "by Gabriel Manhani e Vinicius Macena",
->>>>>>> ebc50c8fba35be2629d4b0740b35c13e5ea401d8
 };
 
 export default function RootLayout({
@@ -28,7 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-<<<<<<< HEAD
       <body className={inter.className}>
         {children}
         <ToastContainer 
@@ -41,11 +30,6 @@ export default function RootLayout({
             draggable 
             pauseOnHover 
         />
-=======
-      <body>
-        <Header title={headerConfig.title} navItems={headerConfig.navItems} />
-        {children}
->>>>>>> ebc50c8fba35be2629d4b0740b35c13e5ea401d8
       </body>
     </html>
   );
