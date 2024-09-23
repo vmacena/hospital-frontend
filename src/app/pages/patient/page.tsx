b24d539e24e3e409ce1a8206f5857780cb3bfb52
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-09-23 02:00:03 -0300

    feat: add navbar on patient page

--- src/app/pages/patient/page.tsx
+++ src/app/pages/patient/page.tsx
@@ -5,26 +5,31 @@ import styles from '@/app/page.module.scss';
 import { usePatientAuth } from '@/app/auth/patient/usePatientAuth';
 import LoginForm from '@/app/components/LoginForm';
 import SignupButton from '@/app/components/SignupButton';
+import Navbar from '@/app/components/NavBar';
+import Topbar from '@/app/components/TopBar';
 
 const PatientPage: React.FC = () => {
   const { susNumber, setSusNumber, handlePatientSubmit } = usePatientAuth();
   const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <div className={styles.containerCenter}>
-      <section className={styles.panel}>
-        <h2>Paciente</h2>
-        <LoginForm
-          formType="susNumber"
-          placeholder="Insira seu número SUS"
-          onSubmit={handlePatientSubmit}
-          isLoading={isLoading}
-          inputValue={susNumber}
-          setInputValue={setSusNumber}
-        />
-        <SignupButton />
-      </section>
-    </div>
+    <>
+      <Navbar />
+      <div className={styles.containerCenter}>
+        <section className={styles.panel}>
+          <h2>Paciente</h2>
+          <LoginForm
+            formType="susNumber"
+            placeholder="Insira seu número SUS"
+            onSubmit={handlePatientSubmit}
+            isLoading={isLoading}
+            inputValue={susNumber}
+            setInputValue={setSusNumber}
+          />
+          <SignupButton />
+        </section>
+      </div>
+    </>
   );
 };
 
